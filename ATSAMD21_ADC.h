2324681485c5a986faa23c5390f7fe9dde89c2c1
Blake Felt <Molorius@users.noreply.github.com>
2017-04-04 11:15:16 -0600

    Added several functions, cleaned up code

--- ATSAMD21_ADC.h
+++ ATSAMD21_ADC.h
@@ -3,42 +3,59 @@
 // Adds some extra functions to the ADC, such as
 // 16 bit and differential mode
 
-#pragma once // only includes this once max
+#ifndef ATSAMD21_ADC_H
+#define ATSAMD21_ADC_H
 
 #ifdef __cplusplus
   extern "C" {
-#endif
+#endif // ifdef __cplusplus
 
 #ifndef ADC_CTRLB_RESSEL_12BIT_Val 
-  #define ADC_CTRLB_RESSEL_8BIT_Val 0x3
-  #define ADC_CTRLB_RESSEL_10BIT_Val 0x2
-  #define ADC_CTRLB_RESSEL_12BIT_Val 0x0
+#define ADC_CTRLB_RESSEL_8BIT_Val   0x03
+#define ADC_CTRLB_RESSEL_10BIT_Val  0x02 // default by Arduino
+#define ADC_CTRLB_RESSEL_12BIT_Val  0x00
 #endif
-#define ADC_CTRLB_RESSEL_16BIT_Val 0x1 // used for averaging mode output
+#define ADC_CTRLB_RESSEL_16BIT_Val  0x01 // used for averaging mode output
 
-#define RSHIFT  0x0 // pg 853
-#define NOSHIFT 0x0
-#define SAMPLES16 0x8 // pg 853
-#define SAMPLES12 0x0
+#define ADC_PIN_TEMP                0x18 // positive mux, pg 870
+#define ADC_PIN_BANDGAP             0x19
+#define ADC_PIN_SCALEDCOREVCC       0x1A
+#define ADC_PIN_SCALEDIOVCC         0x1B
+#define ADC_PIN_DAC                 0x1C
 
-#define ADC_PIN_TEMP 0x18 // positive mux, pg 870
-#define ADC_PIN_BANDGAP 0x19
-#define ADC_PIN_SCALEDCOREVCC 0x1A
-#define ADC_PIN_SCALEDIOVCC 0x1B
-#define ADC_PIN_DAC 0x1C
+#define ADC_PIN_GND                 0x18 // negative mux, pg 869
+#define ADC_PIN_IOGND               0x19
 
-#define ADC_PIN_GND 0x18 // negative mux, pg 869
-#define ADC_PIN_IOGND 0x19
+#define ADC_GAIN_1                  0x00 // pg 868
+#define ADC_GAIN_2                  0x01
+#define ADC_GAIN_4                  0x02
+#define ADC_GAIN_8                  0x03
+#define ADC_GAIN_16                 0x04
+#define ADC_GAIN1_DIV2              0x0F // default by Arduino
 
-#define ADC_GAIN_1 0x0 // pg 868
-#define ADC_GAIN_2 0x1
-#define ADC_GAIN_4 0x2
-#define ADC_GAIN_8 0x3
-#define ADC_GAIN_16 0x4
-#define ADC_GAIN1_DIV2 0xF
+#define ADC_REF_INT1V               0x00 // 1.0V reference, pg 861
+#define ADC_REF_INTVCC0             0x01 // 1/1.48 VDDANA
+#define ADC_REF_INTVCC1             0x02 // 1/2 VDDANA (only for VDDANA > 2.0V) // default
+#define ADC_REF_VREFA               0x03 // external reference
+#define ADC_REF_VREFB               0x04 // external reference
 
-static int _ADCRes = 10; // default for Arduino
-static int _diffmode = 0; // it starts in single-ended mode
+#define ADC_PRESCALER_DIV4          0x00 // pg 864
+#define ADC_PRESCALER_DIV8          0x01
+#define ADC_PRESCALER_DIV16         0x02
+#define ADC_PRESCALER_DIV32         0x03
+#define ADC_PRESCALER_DIV64         0x04
+#define ADC_PRESCALER_DIV128        0x05
+#define ADC_PRESCALER_DIV256        0x06
+#define ADC_PRESCALER_DIV512        0x07 // Arduino default
+
+// NVM Software Calibration Area Mapping, pg 32. Address starting at NVMCTRL_OTP4. 
+// NVM register access code modified from https://github.com/arduino/ArduinoCore-samd/blob/master/cores/arduino/USB/samd21_host.c
+// ADC Linearity Calibration value. Should be written to the CALIB register.
+#define NVM_ADC_LINEARITY_POS         27
+#define NVM_ADC_LINEARITY_SIZE         8
+// ADC Bias Calibration value. Should be written to the CALIB register.
+#define NVM_ADC_BIASCAL_POS           35
+#define NVM_ADC_BIASCAL_SIZE           3
 
 // Taken from Arduino IDE:
 // Wait for synchronization of registers between the clock domains
@@ -121,6 +138,12 @@ uint8_t analogReadExtended(uint8_t bits) {
       return -1;
     break;
    }
+}
+
+// returns the internal pin value of the specified pin, useful
+// for analogDifferentialRaw function
+uint8_t internalPinValue(uint8_t pin) {
+  return g_APinDescription[pin].ulADCChannelNumber;
 }
 
 // modified from Arduino analogRead, can be used in conjunction with analogRead:
@@ -212,6 +235,65 @@ void analogGain(uint8_t gain) {
   syncADC();
 }
 
+// calibrates the bias and linearity based on the nvm register. 
+// NVM register access code modified from https://github.com/arduino/ArduinoCore-samd/blob/master/cores/arduino/USB/samd21_host.c
+// datasheet pages 32 and 882
+void analogCalibrate() { 
+  syncADC();
+  // read NVM register
+  uint32_t adc_linearity = (*((uint32_t *)(NVMCTRL_OTP4) // original position
+          + (NVM_ADC_LINEARITY_POS / 32)) // move to the correct 32 bit window, read value
+          >> (NVM_ADC_LINEARITY_POS % 32)) // shift value to match the desired position
+          & ((1 << NVM_ADC_LINEARITY_SIZE) - 1); // apply a bitmask for the desired size
+
+  uint32_t adc_biascal = (*((uint32_t *)(NVMCTRL_OTP4)
+          + (NVM_ADC_BIASCAL_POS / 32))
+          >> (NVM_ADC_BIASCAL_POS % 32))
+          & ((1 << NVM_ADC_BIASCAL_SIZE) - 1);
+  
+  // write values to CALIB register
+  ADC->CALIB.bit.LINEARITY_CAL = adc_linearity;
+  ADC->CALIB.bit.BIAS_CAL = adc_biascal;
+  syncADC();
+}
+
+// set the analog reference voltage, but with all available options
+// (the Arduino IDE neglects some). The Arduino IDE also changes
+// the gain when analogReference() is used, but this won't. pg 861
+void analogReference2(uint8_t ref) {
+  syncADC();
+  ADC->REFCTRL.bit.REFSEL = ref;
+  syncADC();
+}
+
+// increases accuracy of gain stage by enabling the reference buffer
+// offset compensation. Takes longer to start. pg 861
+void analogReferenceCompensation(uint8_t val) {
+  if(val>0) val = 1; 
+  syncADC();
+  ADC->REFCTRL.bit.REFCOMP = val;
+  syncADC();
+}
+
+// sets the ADC clock relative to the peripheral clock. pg 864
+void analogPrescaler(uint8_t val) {
+  syncADC();
+  ADC->CTRLB.bit.PRESCALER = val;
+  syncADC();
+}
+
+// resets the ADC. pg 860
+// note that this doesn't put back the default values set by the 
+// Arduino IDE. 
+void analogReset() {
+  syncADC();
+  ADC->CTRLA.bit.SWRST = 1; // set reset bit
+  while(ADC->CTRLA.bit.SWRST==1); // wait until it's finished
+  syncADC();
+}
+
 #ifdef __cplusplus
   }
-#endif
+#endif // ifdef __cplusplus
+
+#endif // ifndef ATSAMD21_ADC_H
